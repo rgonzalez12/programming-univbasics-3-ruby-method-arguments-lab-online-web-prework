862f9aae5f696f9b89a3c96b38d90790f013d1fd
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-08-27 02:11:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
@@ -8,6 +9,18 @@ end
 
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+def introduction("Ray")
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language("Ray", "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
+
+def introduction_with_language_optional("Ray", language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+>>>>>>> e7ba86b06396edd33c706dea559cb728b2ccf859
 end 
 
 
